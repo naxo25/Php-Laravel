81990c1ce43d282d908cbc469498c77256c3c860
naxo25 <y.labra.25@gmail.com>
2020-08-20 22:07:06 -0300

    probando

---
+++ ej2.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePersonasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+
+    //TRATA LAS VARIABLES SIN ESPACIOS
+    public function up()
+    {
+        Schema::create('personas', function (Blueprint $table) {
+            $table->increments('Id');
+            $table->string('Nombres');
+            $table->string('ApellidoPaterno');            
+            $table->string('ApellidoMaterno');
+            $table->string('Rut');
+            $table->date('FechadeNacimiento');
+            $table->string('email');
+            $table->string('Contrasena');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('personas');
+    }
+}
